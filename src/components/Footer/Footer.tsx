20ae81222c42ab8a248f066253c40786cdec7c94
Umer Farooque <umermhr@gmail.com>
2025-02-25 18:15:59 +0400

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import { FB, Instagram, Twitter, Youtube } from "@/utils/icons";
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
-const Footer = () => {
+const Footer: React.FC = () => {
     return (
-        <footer>
-        <p>Footer</p>
-        </footer>
+        <Box
+            style={{
+                backgroundColor: "#141414",
+                color: "rgba(255, 255, 255, 0.5)",
+                padding: "0 5rem 3rem 5rem",
+            }}
+        >
+            <Box
+                sx={{
+                    display: { sm: "flex" },
+                    color: "white",
+                    flexDirection: { md: "row" },
+                    gap: "3rem",
+                    marginBottom: "1rem",
+                }}
+            >
+                <FB style={{ fontSize: "2rem" }} />
+                <Instagram style={{ fontSize: "2rem" }} />
+                <Twitter style={{ fontSize: "2rem" }} />
+                <Youtube style={{ fontSize: "2rem" }} />
+            </Box>
+            <Box
+                sx={{
+                    display: { sm: "flex" },
+                    justifyContent: "space-between",
+                }}
+            >
+                <Box>
+                    <Typography>Audio Descriptions</Typography>
+                    <Typography>Investor Relationship</Typography>
+                    <Typography>Policy</Typography>
+                    <Typography>Contact Us</Typography>
+                </Box>
+                <Box>
+                    <Typography>Help Center</Typography>
+                    <Typography>Jobs</Typography>
+                    <Typography>Legal Notices</Typography>
+                    <Typography>Ad Choices</Typography>
+                </Box>
+                <Box>
+                    <Typography>Gift Cards</Typography>
+                    <Typography>Netflix Shop</Typography>
+                    <Typography>Cookies Preference</Typography>
+                </Box>
+                <Box>
+                    <Typography>Media Center</Typography>
+                    <Typography>Terms of Use</Typography>
+                    <Typography>Impressum</Typography>
+                </Box>
+            </Box>
+            <Box>
+                <Typography
+                    sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.5)",
+                        padding: ".2rem .4rem",
+                        my: "2rem",
+                        width: "7rem",
+                    }}
+                >
+                    Service code
+                </Typography>
+            </Box>
+            <Box>© 1997-2025 Nextflix, Inc.</Box>
+        </Box>
     );
-}
+};
 
 export default Footer;
---
+++ src/utils/icons.ts
@@ -0,0 +1,40 @@
+import { BiInfoCircle as Info, BiCaretDown as CaretDown, BiCheck as Tick } from 'react-icons/bi';
+import {
+    IoPlaySharp as Play, IoSearch as Search, IoNotifications as Notifications, IoAddSharp as Add, IoChevronDown as Down, IoLogoLinkedin as Linkedin,
+    IoLogoTwitter as Twitter, IoLogoGithub as Github,
+} from 'react-icons/io5';
+
+import { AiOutlineLike as Like, AiOutlineDislike as Dislike } from 'react-icons/ai';
+
+import { GoMute as Mute, GoUnmute as Unmute } from 'react-icons/go';
+
+import { MdNoAdultContent as Adult, MdHd as HD, MdClose as Close } from 'react-icons/md';
+
+import { TbRating12Plus as Rating } from 'react-icons/tb';
+
+import { FaFacebookF as FB, FaInstagram as Instagram, FaYoutube as Youtube } from 'react-icons/fa';
+
+export {
+    Info,
+    CaretDown,
+    Tick,
+    Play,
+    Search,
+    Notifications,
+    Add,
+    Down,
+    Linkedin,
+    Twitter,
+    Github,
+    Like,
+    Dislike,
+    Mute,
+    Unmute,
+    Adult,
+    HD,
+    Close,
+    Rating,
+    FB,
+    Instagram,
+    Youtube,
+};
